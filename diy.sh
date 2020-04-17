8cd98764829878d50da1f10a91cb7af67ff58efc
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-04-17 18:38:22 +0000

    get ssr plus back

--- diy.sh
+++ diy.sh
@@ -9,3 +9,4 @@
 sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
