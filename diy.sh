8bf9ad3f29dbd08f4e887cfe45a7800274dfdb83
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-04-18 07:00:17 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,3 +10,4 @@ sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_gener
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
