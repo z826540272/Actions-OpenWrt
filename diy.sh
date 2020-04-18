e2c3139c22f496ace25f4a655692878d1ced817e
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-04-18 10:07:40 +0000

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,4 +10,6 @@ sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_gener
 mkdir package/base-files/files/config
 echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+git clone https://github.com/z826540272/openwrt-smartdns.git feeds/packages/net/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
 ./scripts/feeds update -a && ./scripts/feeds install -a
