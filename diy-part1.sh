674c3257cda5d4daaaa5e79d403cbf7aea4d45aa
z826540272 <56025847+z826540272@users.noreply.github.com>
2020-06-05 08:06:14 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,12 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
